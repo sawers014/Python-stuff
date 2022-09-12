f778bb4e1d231711e156c90aeae941ce10dc4dd9
sawers014 <74411308+sawers014@users.noreply.github.com>
2022-09-13 00:07:38 +0200

    optimized
    now it is way faster and its better written, also the total words are now counted

--- automatic typewrite.py
+++ automatic typewrite.py
@@ -1,11 +1,9 @@
-# web browser opener
-#TODO count element with same class name
+#auto typer 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 
 url = "https://typing-speed-test.aoeu.eu/"
 driver = webdriver.Firefox()
@@ -16,17 +14,12 @@ driver.implicitly_wait(0.5)  # make the cookie load
 AGREE = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div/button[2]') #accept the cookie
 AGREE.click()
 
-total_words = driver.find_element(By.CLASS_NAME,"nextword")
-print(total_words)
-for word in range(500):
-    #read current word
-    word = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "currentword"))
-    )
+total_words = driver.find_elements(By.CLASS_NAME,"currentword") + driver.find_elements(By.CLASS_NAME,"nextword")
+
+input_area = driver.find_element(By.ID, "input")
+input_area.click()
+for word in total_words: #read current word
     #put the input
-    input_area = driver.find_element(By.ID, "input")
-    input_area.click()
     input_area.send_keys(word.text)
     input_area.send_keys(Keys.SPACE)
-
 
